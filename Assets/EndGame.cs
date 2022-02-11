4d6f6828fda1d9c7695faeabe011f0687c8ceb91
SHRIJAN GTA <shrijangamer32@gmail.com>
2022-02-11 16:24:50 +0000

    Pro Builder FPS Game

---
+++ Assets/EndGame.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGame : MonoBehaviour
+{
+    public Boolean hit = false;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        hit = true;
+    }
+}
---
+++ Assets/EndText.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class EndText : MonoBehaviour
+{
+    public GameObject BoxCollider;
+    private Text text;
+    void Start()
+    {
+        text = GetComponent<Text>();
+
+        text.color = new Color(0, 0, 0, 0);
+    }
+
+    void Update()
+    {
+        if(BoxCollider.GetComponent<EndGame>().hit == true)
+        {
+            text.color = new Color(1, 0, 0, 1);
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                SceneManager.LoadScene("Level");
+            }
+        }
+    }
+}
